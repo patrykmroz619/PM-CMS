31b312cde5d37404b96ca080f2f54c31e69ea0eb
Patryk Mróz <patrykmroz619@wp.pl>
2020-12-01 01:53:41 +0100

    Fix typo.

--- src/common/index.ts
+++ src/common/index.ts
@@ -1,7 +1,7 @@
 export { GradientBg } from "./GradientBg";
 export { AuthForm } from "./AuthForm";
 export { Button } from "./Button";
-export { P } from "./Parapgraph";
+export { P } from "./Paragraph";
 export { InputWithIcon } from "./InputWithIcon";
 export { PrivateRoute } from "./PrivateRoute";
 export { AuthenticationRoute } from "./AuthenticationRoute";
