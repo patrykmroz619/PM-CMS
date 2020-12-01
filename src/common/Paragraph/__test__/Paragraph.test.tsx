dd5d72bd21d6330823c2c290d25edeb1de2fe481
Patryk Mróz <patrykmroz619@wp.pl>
2020-12-01 12:24:12 +0100

    Write tests for Button and PrivateRoute components.

---
+++ src/common/Paragraph/__test__/Paragraph.test.tsx
@@ -0,0 +1,35 @@
+import { render } from "@testHelpers";
+import React from "react";
+import theme from "../../../style/theme";
+
+import { P } from "../index";
+
+describe("<P />", () => {
+  test("has property text-align: center in styles when props center is passed", () => {
+    const { getByText } = render(<P center>Text</P>);
+
+    const paragraph = getByText(/Text/);
+    expect(paragraph).toHaveStyleRule("text-align", "center");
+  });
+
+  test("has property text-align: left in styles when props center is not passed", () => {
+    const { getByText } = render(<P>Text</P>);
+
+    const paragraph = getByText(/Text/);
+    expect(paragraph).toHaveStyleRule("text-align", "left");
+  });
+
+  test("displays light text color when props light is passed", () => {
+    const { getByText } = render(<P light>Text</P>);
+
+    const paragraph = getByText(/Text/);
+    expect(paragraph).toHaveStyleRule("color", theme.color.light);
+  });
+
+  test("displays dark text color default", () => {
+    const { getByText } = render(<P>Text</P>);
+
+    const paragraph = getByText(/Text/);
+    expect(paragraph).toHaveStyleRule("color", theme.color.dark);
+  });
+});
---
+++ src/common/PrivateRoute/__test__/PrivateRoute.test.tsx
@@ -0,0 +1,65 @@
+import React from "react";
+import { MemoryRouter, Route } from "react-router-dom";
+import * as Redux from "react-redux";
+
+import { renderWithStore } from "@testHelpers";
+import { PrivateRoute } from "../index";
+
+describe("<PrivateRoute />", () => {
+  test("renders children when user is authenticated", () => {
+    const isAuthenticated = true;
+    const spy = jest.spyOn(Redux, "useSelector").mockReturnValue(isAuthenticated);
+
+    const route = "/";
+    const testId = "test";
+    const content = "Content";
+
+    const { getByTestId } = renderWithStore(
+      <MemoryRouter initialEntries={[route]}>
+        <PrivateRoute path={route}>
+          <div data-testid={testId}>{content}</div>
+        </PrivateRoute>
+      </MemoryRouter>,
+      {
+        initialState: {}
+      }
+    );
+
+    const el = getByTestId(testId);
+
+    expect(el.tagName).toBe("DIV");
+    expect(el.textContent).toBe(content);
+
+    spy.mockRestore();
+  });
+
+  test("redirect to /login route when user is not authenticated", () => {
+    const isAuthenticated = false;
+    const spy = jest.spyOn(Redux, "useSelector").mockReturnValue(isAuthenticated);
+
+    const initRoute = "/app";
+    const expectedRoute = "/login";
+    const testId = "test";
+
+    const { getByTestId } = renderWithStore(
+      <MemoryRouter initialEntries={[initRoute]}>
+        <PrivateRoute path={initRoute}>
+          <p>App</p>
+        </PrivateRoute>
+        <Route path={expectedRoute}>
+          <p data-testid={testId}>Login</p>
+        </Route>
+      </MemoryRouter>,
+      {
+        initialState: {}
+      }
+    );
+
+    const el = getByTestId(testId);
+
+    expect(el.tagName).toBe("P");
+    expect(el.textContent).toBe("Login");
+
+    spy.mockRestore();
+  });
+});
