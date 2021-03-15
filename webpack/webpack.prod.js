6a5e7f32b0e61f9d5cca626f947fed01e8ac1451
Patryk Mróz <patrykmroz619@wp.pl>
2021-03-15 15:56:09 +0100

    Disable bundle analyzer.

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,11 +1,9 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const path = require("path");
 const merge = require("webpack-merge");
 const commonConfig = require("./webpack.common");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 module.exports = merge(commonConfig, {
   mode: "production",
-  plugins: [new BundleAnalyzerPlugin(), new CleanWebpackPlugin()]
+  plugins: [new CleanWebpackPlugin()]
 });
