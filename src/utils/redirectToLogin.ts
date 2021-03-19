3017216bc9704824af3a769b7462e74f31269cda
Patryk Mróz <patrykmroz619@wp.pl>
2021-03-19 19:56:47 +0100

    Fix redirect on the login page for hash router.

--- src/utils/redirectToLogin.ts
+++ src/utils/redirectToLogin.ts
@@ -3,6 +3,6 @@ import routes from "@routes";
 export const redirectToLogin = () => {
   const actualHref = window.location.href;
   if (!(actualHref.includes(routes.login) || actualHref.includes(routes.register))) {
-    window.location.href = routes.login;
+    window.location.href = `#${routes.login}`;
   }
 };
