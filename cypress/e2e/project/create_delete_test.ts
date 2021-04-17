a6032f9f0a32be2069f648353b6099a74fee51d7
Patryk Mróz <patrykmroz619@wp.pl>
2021-04-17 22:19:51 +0200

    e2e test for project.

---
+++ cypress/e2e/project/create_delete_test.ts
@@ -0,0 +1,21 @@
+describe("Project", () => {
+  it("can be created and deleted", () => {
+    cy.login();
+    cy.visit("#/panel");
+
+    cy.contains(/add project/i).click();
+
+    cy.get("input").type("My new project");
+    cy.get('button[type="submit"]').click();
+
+    cy.contains(/My new project/).click();
+    cy.wait(1000);
+
+    cy.visit("#/panel/settings");
+
+    cy.contains(/delete project/i).click();
+    cy.contains(/yes/i).click();
+
+    cy.contains(/your project has been deleted/i);
+  });
+});
